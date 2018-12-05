e41a7eb26cae3e33c02e53cf708639dece24a47e
Tenin Sidime <sidime.tenin@gmail.com>
2018-12-05 22:45:11 +0000

    user input

--- js/script.js
+++ js/script.js
@@ -5,16 +5,22 @@
 //GLOBAL VARIABLES
 /* global $ */
 
-var userchoice="";
+var userChoice="";
 var computerChoice="";
 var winner="";
 var randomNumber=0;
  
- $(function(){
-     $("#shoot").click(function(){
-         userchoice= $("input").val();
-     });
- });
+$(document).ready(function(){
+    $("#shoot").click(function(){
+        userChoice= $("#input").val();
+        $("#userChoice").text(userChoice)
+        console.log(userChoice);
+        
+        
+        //var randomNumber=Math.random();
+        
+    });
+});
 
 
 
